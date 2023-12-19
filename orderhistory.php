c46d9178a6355aec06e6004d13daa14e4b078ae2
Davidjgn <davidjgn11@gmail.com>
2023-12-19 14:50:41 -0800

    Add order history page, database update pending.

---
+++ orderhistory.php
@@ -0,0 +1,40 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
+include("./connect.php");
+
+$conn = new mysqli($dbServer, $dbUser, $dbPass, $dbName);
+
+if ($conn->connect_error) {
+    die(json_encode(['error' => 'Database connection error.']));
+}
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        $userid = 1017;
+        $selectQuery = "SELECT * FROM order_tb WHERE userid=$userid";
+        $data = $conn->query($selectQuery);
+        $outData = [];
+        if($data->num_rows > 0){
+            while($row = $data->fetch_assoc()){               
+                array_push($outData,$row);
+            }
+            echo json_encode($outData);
+         }
+         $conn->close();
+        break;
+    case 'POST':
+        
+        break;
+    case 'PUT':
+        
+        break;
+    case 'DELETE':
+        
+        break;
+    default:
+        die(json_encode(['error' => 'Invalid request method.']));
+}
+?>
